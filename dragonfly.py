ce695060727bbde91a3b9532824729842a0993fc
Corbin <forstupidityonly@gmail.com>
2022-04-04 13:05:42 -0500

    added react front end of sorts

--- dragonfly.py
+++ dragonfly.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/testing")
+def testing():
+    return {"testing": ["test1", "test2", "test3"]}
+
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
