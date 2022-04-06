0523f509b8d73d8db3df8b5830ae800f68ed9932
mlaizure <maddi.laizure@gmail.com>
2022-04-05 23:08:40 -0500

    Adding basic commit message analysis

--- dragonfly.py
+++ dragonfly.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from commit_analysis import analysis
+
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@app.route("/dashboard")
+def dashboard():
+    return analysis()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
