9378c6281955290619010ddfb5499ff3d399e184
mlaizure <maddi.laizure@gmail.com>
2022-04-15 16:33:48 -0500

    Working version grabbing first installation

--- commit_analysis.py
+++ commit_analysis.py
@@ -1,17 +1,19 @@
 import os
 import json
-from github import Github, GithubIntegration
+from github import Github, GithubIntegration, Installation
 import matplotlib
 from matplotlib.figure import Figure
+import requests
 
-app_id = '187180'
+app_id = 187180
 
 if os.getenv("PRIVATE_KEY"):
     app_key = os.getenv("PRIVATE_KEY")
 
 else:
     with open(
-            os.path.normpath(os.path.expanduser('./dragonfly-analytics.2022-04-05.private-key.pem')),
+            os.path.normpath(os.path.expanduser(
+                './dragonfly-analytics.2022-04-05.private-key.pem')),
             'r'
     ) as cert_file:
         app_key = cert_file.read()
@@ -23,32 +25,80 @@ git_integration = GithubIntegration(
 )
 
 
-def analysis():
-    OWNER="mlaizure"
-    REPO="holbertonschool-web_react"
+def getInstallationById(git_integration, installationId):
 
-    git_connection = Github(
-        login_or_token=git_integration.get_access_token(
-            git_integration.get_installation(OWNER, REPO).id
-        ).token
-    )
-    repo = git_connection.get_repo(f"{OWNER}/{REPO}")
+    token = git_integration.get_access_token(installationId).token
 
-    gh_commits = repo.get_commits("main")
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.machine-man-preview+json",
+        "User-Agent": "PyGithub/Python",
+    }
+    repos_url = f"{git_integration.base_url}/installation/repositories"
+    repos_response = requests.get(repos_url, headers=headers)
+    repos_response_dict = repos_response.json()
+    first_repo = repos_response_dict["repositories"][0]
+    owner = first_repo["owner"]["login"]
+    repo_name = first_repo["name"]
+
+    return {
+        "installation": git_integration.get_installation(owner, repo_name),
+        "owner": owner,
+        "repo_name": repo_name,
+    }
+
+
+def installation(user_connection):
+
+    installations = user_connection.get_user().get_installations()
+    inst = [inst for inst in installations if inst.app_id == app_id][0]
+
+    return getInstallationById(git_integration, inst.id)
+
+
+def analysis(inst):
+
+    git_connection = Github(login_or_token=git_integration.get_access_token(
+        inst["installation"].id).token)
+
+    owner = inst["owner"]
+    repo_name = inst["repo_name"]
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+    gh_commits = repo.get_commits("master")
 
     num_fixes_by_file = {}
     for gh_commit in gh_commits:
         if is_keyword(gh_commit.commit.message):
             for f in gh_commit.files:
-                if f.filename in num_fixes_by_file.keys():
-                    num_fixes_by_file[f.filename] += 1
-                else:
-                    num_fixes_by_file[f.filename] = 1
-    #print(json.dumps(num_fixes_by_file, indent=4, sort_keys=True))
+                if not is_ignored(f.filename):
+                    if f.filename in num_fixes_by_file.keys():
+                        num_fixes_by_file[f.filename] += 1
+                    else:
+                        num_fixes_by_file[f.filename] = 1
     return num_fixes_by_file
 
-def create_chart():
-    data = analysis()
+
+def is_keyword(msg):
+    keywords = ['fix', 'bug', 'issue']
+    if any(keyword in msg.lower() for keyword in keywords):
+        return True
+    return False
+
+
+def is_ignored(path):
+    ignore_ext = ['.json', '.md', '.ps', '.eps', '.txt', '.xml', '.xsl',
+                  '.rss', '.xslt', '.xsd', '.wsdl', '.wsf', '.yaml', '.yml',
+                  '~', '#', '.png', '.jpg', '.jpeg', '.gif']
+    for ext in ignore_ext:
+        l = len(ext)
+        if path[-l:] == ext:
+            return True
+    return False
+
+
+def create_chart(inst):
+    data = analysis(inst)
 
     files = []
     fixes = []
@@ -62,24 +112,21 @@ def create_chart():
         fontsize = 10
     else:
         fontsize = 15
-    
-    ax1.pie(fixes, labels=files, autopct='%1.1f%%', shadow=True, radius=1.5, textprops={'fontsize': fontsize}, labeldistance=1.06, pctdistance=0.80)
-    wedges = [patch for patch in ax1.patches if isinstance(patch, matplotlib.patches.Wedge)]
+
+    ax1.pie(fixes, labels=files, autopct='%1.1f%%', shadow=True,
+            radius=1.5, textprops={'fontsize': fontsize},
+            labeldistance=1.06, pctdistance=0.80)
+    wedges = [patch for patch in ax1.patches if isinstance(
+        patch, matplotlib.patches.Wedge)]
     for w in wedges:
         w.set_linewidth = 2
         w.set_edgecolor("black")
 
     ax1.axis('equal')
-    
+
     dpi = 200
     width = 1920
     height = 1080
     fig1.set_size_inches(width / dpi, height / dpi)
 
     return fig1
-
-def is_keyword(msg):
-    keywords = ['fix', 'bug', 'issue']
-    if any(keyword in msg.lower() for keyword in keywords):
-        return True
-    return False
--- dragonfly.py
+++ dragonfly.py
@@ -1,44 +1,58 @@
-from flask import Response, Flask, jsonify, send_from_directory, render_template
-from commit_analysis import analysis, create_chart
+import json
+from flask import (
+    Response, Flask, jsonify, send_from_directory,
+    render_template, redirect, url_for
+)
+from flask_dance.contrib.github import make_github_blueprint, github
+from commit_analysis import (
+    analysis, create_chart, git_integration, installation
+)
 from io import BytesIO
 import base64
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from dotenv import load_dotenv
+import os
+from github import Github
+
+load_dotenv()
+
+app_id = 187180
 
 app = Flask(__name__,
             template_folder="build",
             static_url_path="",
             static_folder="build")
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
+app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get(
+    "GITHUB_OAUTH_CLIENT_SECRET")
+github_bp = make_github_blueprint()
+app.register_blueprint(github_bp, url_prefix="/login")
 
-@app.route("/testing")
-def testing():
-    fig = create_chart()
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    # return 'data:image/png;base64,{data}'
-    return Response(buf, mimetype="application/png")
 
 @app.route("/")
 def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
     return render_template("index.html")
+
 
 @app.route("/dashboard")
 def dashboard():
-    return analysis()
+    user_connection = Github(github.token["access_token"])
+    return analysis(installation(user_connection))
+
 
 @app.route("/chart")
 def chart():
-    fig = create_chart()
+    user_connection = Github(github.token["access_token"])
+    fig = create_chart(installation(user_connection))
     buf = BytesIO()
-    # fig.savefig(buf, format="png")
     FigureCanvas(fig).print_png(buf)
-    # data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    # return f"<img src='data:image/png;base64,{data}'/>"
     return Response(buf.getvalue(), mimetype="image/png")
-    # return Response(buf, mimetype="image/png")
-    # return buf
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
