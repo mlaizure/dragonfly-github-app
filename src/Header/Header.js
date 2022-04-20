6359a5545e88005de01b55e36d189760beec1d15
Corbin <forstupidityonly@gmail.com>
2022-04-20 14:07:25 -0500

    download link mabye

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -11,9 +11,15 @@ function Header() {
         </div>
         <div className="links">
           <h1><a href="https://github.com/mlaizure/dragonfly" target="_blank" rel="noopener noreferrer">Dragonfly Local Tool</a></h1>
-          <h1><a href="#">Download Analytics</a></h1>
+          <h1><a id="download_link" href="" download="dragonflyData">Download Analytics</a></h1>
         </div>
       </div>
+      <script>
+        var jsonse = JSON.stringify(data);
+        var blob = new Blob([jsonse], {type: "application/json"});
+        var url  = URL.createObjectURL(blob);
+        document.getElementById('download_link').href = url;
+      </script>
     </React.Fragment>
   )
 }
